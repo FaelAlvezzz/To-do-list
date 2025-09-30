265ae69a60e563bc48665fae60a53e6eb8334ff6
Rafael Alves <FaelAlvezzz@Gmail.com>
2025-09-30 08:51:06 -0300

    Att App.jsx

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,9 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import {useStates} from react;
 
 function App() {
   const [tarefas, setTarefas] = useState([]);
